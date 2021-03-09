ed98416f5b815d9f8c43b9ddd50f73212222edbe
criselle albor <alborcriselle10@gmail.com>
2021-03-09 14:11:08 +0800

    display courses on courses.html from database using fetch and using cards

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -9,4 +9,23 @@ fetch('http://localhost:4000/api/courses/')
     })
     .then(data => {
         console.log(data)
+        // <li>name</li>
+        let courseContainer = document.querySelector('#courseContainer');
+        let courseData = data.map(elem => {
+            return `
+	            <div class="col-md-6 my-3">
+		            <div class="card">
+	    		        <div class="card-body">
+	            			<h5 class="card-title">${elem.name}</h5>
+	            			<p class="card-text text-right">&#8369; ${elem.price}</p>
+	            			<p class="card-text">${elem.description}</p>
+	           			</div>
+	            	</div>
+	            </div>
+	        `
+        })
+
+        console.log(courseData.join(""))
+
+        courseContainer.innerHTML = courseData.join("");
     })
