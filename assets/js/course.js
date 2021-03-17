bf6d65c5618dfdc4a7400c8428d6976fa7f58bd9
criselle albor <alborcriselle10@gmail.com>
2021-03-17 08:47:15 +0800

    aadd enroll course function

--- assets/js/course.js
+++ assets/js/course.js
@@ -15,4 +15,31 @@ fetch(url)
         courseName.innerHTML = data.name
         courseDesc.innerHTML = data.description
         coursePrice.innerHTML = data.price
+
+
+        document.querySelector('#enrollButton').addEventListener('click', () => {
+            fetch(`http://localhost:4000/api/users/enroll`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage['token']}`
+                    },
+                    body: JSON.stringify({
+                        courseId: courseId
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data === true) {
+                        // notify the user that enrollment is successful
+                        alert("thank you for enrolling! see you!");
+                        // redirect user to courses page
+                        window.location.replace('./courses.html');
+                    } else {
+                        //notify user that enrollment failed
+                        alert("something went wrong");
+                    }
+                })
+        })
     })
